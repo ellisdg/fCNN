1eb07c79975cacc27c1366ede4af3af1700eee48
ellisdg <dgellis90@gmail.com>
2019-03-19 15:11:29 -0500

    update trial running script

--- trials/run_trial.py
+++ trials/run_trial.py
@@ -6,13 +6,17 @@ from fcnn.train import run_training
 
 
 if __name__ == '__main__':
-    config_filename = sys.argv[0]
-    model_filename = sys.argv[1]
-    training_log_filename = sys.argv[2]
+    config_filename = sys.argv[1]
+    print("Config: ", config_filename)
+    model_filename = sys.argv[2]
+    print("Model: ", model_filename)
+    training_log_filename = sys.argv[3]
+    print("Log: ", training_log_filename)
     try:
-        multiprocessing_config_filename = sys.argv[3]
+        multiprocessing_config_filename = sys.argv[4]
         with open(multiprocessing_config_filename, 'r') as opened_filename:
             multiprocessing_config = json.load(opened_filename)
+        print("MP Config: ", multiprocessing_config_filename)
     except IndexError:
         multiprocessing_config = dict()
 

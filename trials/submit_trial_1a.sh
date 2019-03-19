1adce7c4cd9323fe159dafbadf83240f7f2e49c9
David Ellis <david.ellis@unmc.edu>
2019-03-19 14:01:06 -0500

    Fixes memory requested to be viable

--- trials/submit_trial_1a.sh
+++ trials/submit_trial_1a.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:2
 #SBATCH --constraint=gpu_p100
 #SBATCH --ntasks-per-node=8
-#SBATCH --mem-per-cpu=8100       # Maximum memory required per CPU (in megabytes)
+#SBATCH --mem-per-cpu=8000       # Maximum memory required per CPU (in megabytes)
 #SBATCH --error=/work/aizenberg/dgellis/job.%J.err
 #SBATCH --output=/work/aizenberg/dgellis/job.%J.out
 

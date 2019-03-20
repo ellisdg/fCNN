90fcb7adb5af4328c728f6e778c8c31f61359c58
David Ellis <david.ellis@unmc.edu>
2019-03-20 10:12:47 -0500

    fixes k40 config

--- trials/submit_trial_2_k40.sh
+++ trials/submit_trial_2_k40.sh
@@ -4,14 +4,14 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:4
 #SBATCH --constraint=gpu_k40
-#SBATCH --ntasks-per-node=32
-#SBATCH --mem-per-cpu=2000       # Maximum memory required per CPU (in megabytes)
+#SBATCH --ntasks-per-node=16
+#SBATCH --mem-per-cpu=4000       # Maximum memory required per CPU (in megabytes)
 #SBATCH --error=/work/aizenberg/dgellis/fCNN/logs/job.%J.err
 #SBATCH --output=/work/aizenberg/dgellis/fCNN/logs/job.%J.out
 
 module load cuda
 module load anaconda
-source activate fcnn
+source activate fcnn-1.12
 
 TRIAL=trial_2a
 CONFIG=/home/aizenberg/dgellis/fCNN/data/trial2_config.json

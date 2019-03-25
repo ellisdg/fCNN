e2f1c31d254018c9f20026cff8ee57275f4d7c7c
David Ellis <david.ellis@unmc.edu>
2019-03-25 17:41:08 -0500

    Trial 4

--- trials/run_trial_4a_local.sh
+++ trials/run_trial_4a_local.sh
@@ -3,7 +3,7 @@
 TRIAL=trial_4a
 CONFIG=/home/neuro-user/PycharmProjects/fCNN/data/trial4_local_config.json
 HCC_CONFIG=/home/neuro-user/PycharmProjects/fCNN/data/local_config.json
-MODEL=/home/neuro-user/PycharmProjects/fCNN/trials/${TRIAL}/model_${TRIAL}.h5
-LOG=/home/neuro-user/PycharmProjects/fCNN/trials/${TRIAL}/log_${TRIAL}.csv
+MODEL=/home/neuro-user/PycharmProjects/fCNN/trials/models/model_${TRIAL}.h5
+LOG=/home/neuro-user/PycharmProjects/fCNN/trials/training_logs/log_${TRIAL}.csv
 
 python /home/neuro-user/PycharmProjects/fCNN/trials/run_trial.py ${CONFIG} ${MODEL} ${LOG} ${HCC_CONFIG}

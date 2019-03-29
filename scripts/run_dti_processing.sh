3e382e2b4dff3dd4370277ae8698e544b2559b14
ellisdg <dgellis90@gmail.com>
2019-03-29 12:15:14 -0500

    calls a specific version of python

--- scripts/run_dti_processing.sh
+++ scripts/run_dti_processing.sh
@@ -11,4 +11,4 @@ source activate dti
 
 OUTPUT="$(which python)"
 echo "${OUTPUT}"
-python /home/aizenberg/dgellis/fCNN/scripts/process_dti.py ${1} ${2}
+/home/aizenberg/dgellis/.conda/envs/dti/bin/python /home/aizenberg/dgellis/fCNN/scripts/process_dti.py ${1} ${2}

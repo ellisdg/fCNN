b3a39b95f034edb36b13b4adfc15b160846bb087
David Ellis <david.ellis@unmc.edu>
2019-03-29 14:27:29 -0500

    Trial 7

---
+++ trials/submit_trial_7_p100.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#SBATCH --time=168:00:00          # Run time in hh:mm:ss
+#SBATCH --job-name=trial_7
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --constraint=gpu_p100
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem-per-cpu=8000       # Maximum memory required per CPU (in megabytes)
+#SBATCH --error=/work/aizenberg/dgellis/job.%J.err
+#SBATCH --output=/work/aizenberg/dgellis/job.%J.out
+
+module load cuda
+module load anaconda
+source activate fcnn-1.12
+
+TRIAL=trial_5
+CONFIG=/home/aizenberg/dgellis/fCNN/data/${TRIAL}_config.json
+HCC_CONFIG=/home/aizenberg/dgellis/fCNN/data/hcc_p100_config.json
+MODEL=/work/aizenberg/dgellis/fCNN/model_${TRIAL}.h5
+LOG=/work/aizenberg/dgellis/fCNN/log_${TRIAL}.csv
+
+export PYTHONPATH=/home/aizenberg/dgellis/3DUnetCNN:$PYTHONPATH
+
+python /home/aizenberg/dgellis/fCNN/trials/run_trial.py ${CONFIG} ${MODEL} ${LOG} ${HCC_CONFIG}

1b3eee81dfce345acdda7bca31668345b001ae9c
ellisdg <dgellis90@gmail.com>
2019-04-02 16:11:59 -0500

    Sets up low quality trial 1

---
+++ trials/submit_trial_lowq_1_k40.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#SBATCH --time=168:00:00          # Run time in hh:mm:ss
+#SBATCH --job-name=trial_lowq_1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:4
+#SBATCH --constraint=gpu_k40
+#SBATCH --ntasks-per-node=16
+#SBATCH --mem-per-cpu=4000       # Maximum memory required per CPU (in megabytes)
+#SBATCH --error=/work/aizenberg/dgellis/fCNN/logs/job.%J.err
+#SBATCH --output=/work/aizenberg/dgellis/fCNN/logs/job.%J.out
+
+module load cuda
+module load anaconda
+source activate fcnn-1.12
+
+TRIAL=trial_lowq_1
+CONFIG=/home/aizenberg/dgellis/fCNN/data/${TRIAL}_config.json
+HCC_CONFIG=/home/aizenberg/dgellis/fCNN/data/hcc_k40_config.json
+MODEL=/work/aizenberg/dgellis/fCNN/model_${TRIAL}.h5
+LOG=/work/aizenberg/dgellis/fCNN/log_${TRIAL}.csv
+
+export PYTHONPATH=/home/aizenberg/dgellis/3DUnetCNN:$PYTHONPATH
+
+python /home/aizenberg/dgellis/fCNN/trials/run_trial.py ${CONFIG} ${MODEL} ${LOG} ${HCC_CONFIG}

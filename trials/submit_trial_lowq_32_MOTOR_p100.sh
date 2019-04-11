c98e03ca616efa93164a767b174920baa07c24d1
ellisdg <dgellis90@gmail.com>
2019-04-11 11:38:35 -0500

    LowQ Motor ResNet-32

---
+++ trials/submit_trial_lowq_32_MOTOR_p100.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#SBATCH --time=168:00:00          # Run time in hh:mm:ss
+#SBATCH --job-name=LQ32MOTOR
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --constraint=gpu_p100
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem-per-cpu=8000       # Maximum memory required per CPU (in megabytes)
+#SBATCH --error=/work/aizenberg/dgellis/fCNN/logs/job.%J.err
+#SBATCH --output=/work/aizenberg/dgellis/fCNN/logs/job.%J.out
+
+module load cuda
+module load anaconda
+source activate fcnn-1.12
+
+TRIAL=trial_lowq_32_MOTOR
+CONFIG=/home/aizenberg/dgellis/fCNN/data/${TRIAL}_config.json
+HCC_CONFIG=/home/aizenberg/dgellis/fCNN/data/hcc_p100_config.json
+MODEL=/work/aizenberg/dgellis/fCNN/model_${TRIAL}.h5
+LOG=/work/aizenberg/dgellis/fCNN/log_${TRIAL}.csv
+
+export PYTHONPATH=/home/aizenberg/dgellis/3DUnetCNN:$PYTHONPATH
+export KERAS_BACKEND=tensorflow
+
+/home/aizenberg/dgellis/.conda/envs/fcnn-1.12/bin/python /home/aizenberg/dgellis/fCNN/trials/run_trial.py ${CONFIG} ${MODEL} ${LOG} ${HCC_CONFIG}

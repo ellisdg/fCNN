7bfb55f8ae73b8d2a3b92174ca5785d5041adf10
David Ellis <david.ellis@unmc.edu>
2019-04-15 09:39:21 -0500

    FIX: fixes fmri parcellation

--- scripts/compile_tfmri_parcellation_data.py
+++ scripts/compile_tfmri_parcellation_data.py
@@ -27,9 +27,10 @@ def main():
         for motor_id, lang_id in zip(motor_config[subset], lang_config[subset]):
             assert motor_id == lang_id
             subject_id = motor_id
-            data[subject_id].append(subject_id)
+            data["subject_id"].append(subject_id)
             results_dir = "{hcp_dir}/{subject_id}/MNINonLinear/Results".format(hcp_dir=hcp_dir, subject_id=subject_id)
             for smoothing_level in smoothing_levels:
+                smoothing_name = "_s{}_".format(smoothing_level)
                 for task_type, config in zip(task_names, (motor_config, lang_config)):
                     task_results_dir = "{results_dir}/tfMRI_{task_name}/tfMRI_{task_name}_hp200_s{smoothing_level}_level2_MSMAll.feat/".format(
                         results_dir=results_dir,
@@ -41,11 +42,15 @@ def main():
                         task_name=task_type,
                         smoothing_level=smoothing_level)
                     parcellated_filename = "{cifti_basename}.pscalar.nii".format(cifti_basename=cifti_basename)
+                    print(parcellated_filename)
                     pscalar = nib.load(parcellated_filename)
 
                     parcel_names = extract_parcellated_scalar_parcel_names(pscalar)
                     for raw_target_name in config['metric_names']:
                         target_name = raw_target_name.format(subject_id)
+                        if smoothing_name not in target_name:
+                            target_name = target_name.replace(("_s2_", "_s4_")[[4, 2].index(smoothing_level)],
+                                                              smoothing_name)
                         parcel_data = extract_parcellated_scalar_map(pscalar, target_name)
                         task_name = parse_metric_name(target_name)
                         for index, parcel_name in enumerate(parcel_names):
@@ -64,6 +69,6 @@ def parse_metric_name(metric_name):
     return "_".join((task_type, task_name))
 
 
-if __name__ is "__main__":
+if __name__ == "__main__":
     main()
 

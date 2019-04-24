7fd0925c036a2dbf603f0ac71c18648f16db5137
ellisdg <dgellis90@gmail.com>
2019-04-24 14:04:08 -0500

    Fixes bash to sbatch

--- trials/submit_predictions.py
+++ trials/submit_predictions.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     config = load_json(sys.argv[1])
     script = sys.argv[2]
     for subject_id in config['validation']:
-        subprocess.call(['bash', script, subject_id])
+        subprocess.call(['sbatch', script, subject_id])

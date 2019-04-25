b407c5004cb2b963edb7ce79f7b843d039b5e521
ellisdg <dgellis90@gmail.com>
2019-04-25 10:06:29 -0500

    Submit predictions

--- trials/submit_predictions.py
+++ trials/submit_predictions.py
@@ -1,11 +1,12 @@
 from fcnn.utils.utils import load_json
 import sys
 import subprocess
-
+import os
 
 if __name__ == '__main__':
-    config = load_json(sys.argv[1])
-    script = sys.argv[2]
-    model = sys.argv[3]
+    config_filename = os.path.abspath(sys.argv[1])
+    config = load_json(config_filename)
+    script = os.path.abspath(sys.argv[2])
+    model_filename = os.path.abspath(sys.argv[3])
     for subject_id in config['validation']:
-        subprocess.call(['sbatch', script, subject_id, model, config])
+        subprocess.call(['sbatch', script, subject_id, model_filename, config_filename])

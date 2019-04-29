92783a166e0de9f2584000b81d2e4dc84ac6d151
ellisdg <dgellis90@gmail.com>
2019-04-29 11:19:09 -0500

    fixes dscalar filename

--- scripts/run_prediction_parcellation.py
+++ scripts/run_prediction_parcellation.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     label_filename = "/home/aizenberg/dgellis/Q1-Q6_RelatedValidation210.CorticalAreas_dil_Final_Final_Areas_Group_Colors.32k_fs_LR.dlabel.nii"
     for gifti_left_filename in glob.glob("/work/aizenberg/dgellis/fCNN/predictions/*.L.*.func.gii"):
         gifti_right_filename = gifti_left_filename.replace(".L.", ".R.")
-        cifti_filename = gifti_left_filename.replace(".L.", ".").replace(".func.gii", "dscalar.nii")
+        cifti_filename = gifti_left_filename.replace(".L.", ".").replace(".func.gii", ".dscalar.nii")
 
         if not os.path.exists(cifti_filename):
             cmd_args = ["/work/aizenberg/dgellis/tools/workbench/bin_rh_linux64/wb_command",

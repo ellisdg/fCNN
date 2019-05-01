a5484359a2cd67c6d338a5cacf3c62f4f2cdc05e
David Ellis <david.ellis@unmc.edu>
2019-05-01 16:43:34 -0500

    Adds local bash scripts

---
+++ trials/run_trial_34_MOTOR_local.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+TRIAL=trial_34_MOTOR
+CONFIG=/home/neuro-user/PycharmProjects/fCNN/data/${TRIAL}_local_config.json
+HCC_CONFIG=/home/neuro-user/PycharmProjects/fCNN/data/local_config.json
+MODEL=/home/neuro-user/PycharmProjects/fCNN/trials/models/model_${TRIAL}.h5
+LOG=/home/neuro-user/PycharmProjects/fCNN/trials/training_logs/log_${TRIAL}.csv
+
+python /home/neuro-user/PycharmProjects/fCNN/trials/run_trial.py ${CONFIG} ${MODEL} ${LOG} ${HCC_CONFIG}

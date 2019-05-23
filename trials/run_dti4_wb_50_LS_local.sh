68d38fb49b0a7ab7e4fae7e9d1bcf1a54fedff6d
David Ellis <david.ellis@unmc.edu>
2019-05-23 16:06:17 -0500

    Fixes trial name

--- trials/run_dti4_wb_50_LS_local.sh
+++ trials/run_dti4_wb_50_LS_local.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TRIAL=dti4_wb_50_34_LS
+TRIAL=dti4_wb_50_LS
 CONFIG=/home/neuro-user/PycharmProjects/fCNN/data/${TRIAL}_local_config.json
 HCC_CONFIG=/home/neuro-user/PycharmProjects/fCNN/data/local_config.json
 MODEL=/home/neuro-user/PycharmProjects/fCNN/trials/models/model_${TRIAL}.h5

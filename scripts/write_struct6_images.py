1c57e7a4b1f0177bda6105da302cf467b5cc85f6
ellisdg <dgellis90@gmail.com>
2019-05-30 13:15:03 -0500

    sets spacing to 1.25

--- scripts/write_struct6_images.py
+++ scripts/write_struct6_images.py
@@ -2,7 +2,10 @@ import os
 import json
 import numpy as np
 import nibabel as nib
-from nilearn.image import crop_img, reorder_img, resample_to_img
+from nilearn.image import resample_to_img
+from fcnn.utils.utils import normalize_image_data
+from unet3d.utils.nilearn_utils import crop_img, reorder_affine
+from unet3d.utils.utils import resample
 
 
 def load_json(filename):
@@ -46,16 +49,22 @@ def main():
                                            "hcc_p100_config.json"))
     for subject_id in config['validation'] + config["training"]:
         subject_dir = os.path.join(system_config['directory'], subject_id)
-        output_filename = os.path.join(subject_dir, "T1w", "struct6.nii.gz")
+        output_filename = os.path.join(subject_dir, "T1w", "struct6_1.25_normalized.nii.gz")
         print(output_filename)
         if not os.path.exists(output_filename):
+            mask_filename = os.path.join(subject_dir, "T1w", "fs_brainmask.nii.gz")
+            mask_image = nib.load(mask_filename)
             feature_filenames = [os.path.join(subject_dir, fbn) for fbn in config["feature_basenames"]]
-            image = combine_images([nib.load(fn) for fn in feature_filenames],
+            feature_images = [nib.load(fn) for fn in feature_filenames][::-1]
+            image = combine_images(feature_images,
                                    axis=3,
                                    resample_unequal_affines=True,
                                    interpolation="continuous")
-            image = reorder_img(image, resample="continuous")
-            image = crop_img(image)
+            resampled_mask = resample_to_img(mask_image, image, interpolation="nearest")
+            crop_affine, crop_shape = crop_img(resampled_mask, return_affine=True, pad=False)
+            reordered_affine = reorder_affine(crop_affine, crop_shape)
+            image = resample(image, reordered_affine, crop_shape, interpolation="continuous")
+            image = image.__class__(normalize_image_data(image.get_data()), image.affine)
             image.to_filename(output_filename)
 
 

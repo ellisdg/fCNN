531c15fe08bda0a2f5c50ba3a76f8718c99337c7
ellisdg <dgellis90@gmail.com>
2019-06-28 17:55:52 -0500

    fixes file path

--- trials/run_predictions_p100.sh
+++ trials/run_predictions_p100.sh
@@ -6,4 +6,4 @@ MCONF=/home/aizenberg/dgellis/fCNN/data/hcc_p100_config.json
 ODIR=/work/aizenberg/dgellis/fCNN/predictions
 BIAS=/work/aizenberg/dgellis/HCP/HCP_1200/validation_tfMRI_LANGUAGE_level2_hp200_s2.dscalar.nii
 SID=198855
-/home/aizenberg/dgellis/.conda/envs/fcnn-1.12/bin/python /home/aizenberg/dgellis/fCNN/scripts/make_whole_brain_predictions.py ${CONF} ${MODEL} ${MCONF} ${ODIR} ${BIAS} ${SID}
+/home/aizenberg/dgellis/.conda/envs/fcnn-1.12/bin/python /home/aizenberg/dgellis/fCNN/fcnn/scripts/make_whole_brain_predictions.py ${CONF} ${MODEL} ${MCONF} ${ODIR} ${BIAS} ${SID}

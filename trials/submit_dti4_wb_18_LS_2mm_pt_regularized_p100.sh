a7a54dda9347350c30a6609957b68e20930f335a
ellisdg <dgellis90@gmail.com>
2019-07-15 17:39:40 -0500

    FIX: switches path of script

--- trials/submit_dti4_wb_18_LS_2mm_pt_regularized_p100.sh
+++ trials/submit_dti4_wb_18_LS_2mm_pt_regularized_p100.sh
@@ -19,4 +19,4 @@ HCC_CONFIG=/home/aizenberg/dgellis/fCNN/data/hcc_p100_config.json
 MODEL=/work/aizenberg/dgellis/fCNN/model_${TRIAL}.h5
 LOG=/work/aizenberg/dgellis/fCNN/log_${TRIAL}.csv
 
-/home/aizenberg/dgellis/.conda/envs/fcnn-1.12/bin/python /home/aizenberg/dgellis/fCNN/trials/run_trial.py ${CONFIG} ${MODEL} ${LOG} ${HCC_CONFIG}
+/home/aizenberg/dgellis/.conda/envs/fcnn-1.12/bin/python /home/aizenberg/dgellis/fCNN/fcnn/scripts/run_trial.py ${CONFIG} ${MODEL} ${LOG} ${HCC_CONFIG}

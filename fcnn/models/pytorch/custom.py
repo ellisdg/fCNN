9ff8a3e7410947e0f28cd2c5a47623b8cde8a78a
ellisdg <dgellis90@gmail.com>
2019-07-17 11:55:36 -0500

    maybe slim

--- fcnn/models/pytorch/custom.py
+++ fcnn/models/pytorch/custom.py
@@ -6,7 +6,7 @@ from .resnet import conv1x1x1
 
 
 class VariationalAutoEncoder(nn.Module):
-    def __init__(self, input_shape, n_features, base_width=32, encoder_blocks=None, decoder_blocks=None,
+    def __init__(self, input_shape, n_features, base_width=16, encoder_blocks=None, decoder_blocks=None,
                  feature_dilation=2, downsampling_stride=2, n_reduced_latent_feature_maps=16, vae_features=128,
                  interpolation_mode="trilinear"):
         super(VariationalAutoEncoder, self).__init__()

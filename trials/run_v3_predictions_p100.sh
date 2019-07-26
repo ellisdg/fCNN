b080f46be11624f6c5efa87e7f1ac4bc84a1d50d
ellisdg <dgellis90@gmail.com>
2019-07-26 10:33:41 -0500

    Adds script to run v3 trials

---
+++ trials/run_v3_predictions_p100.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+TRIAL=${1}
+SYSTEM=${2}
+MODEL=/work/aizenberg/dgellis/fCNN/model_${TRIAL}.pt
+CONF=/home/aizenberg/dgellis/fCNN/data/${TRIAL}_config.json
+MCONF=/home/aizenberg/dgellis/fCNN/data/hcc_${SYSTEM}_config.json
+ODIR=/work/aizenberg/dgellis/fCNN/predictions
+BIAS=/work/aizenberg/dgellis/HCP/HCP_1200/v3_training_tfMRI_LANGUAGE_level2_hp200_s2.dscalar.nii
+SID=100206
+/home/aizenberg/dgellis/.conda/envs/fcnn-1.12/bin/python /home/aizenberg/dgellis/fCNN/fcnn/scripts/make_whole_brain_predictions.py ${CONF} ${MODEL} ${MCONF} ${ODIR} ${BIAS} ${SID}

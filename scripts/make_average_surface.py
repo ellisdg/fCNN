c932c8b31bef233d8b53ee5c411e02686dc4bf88
ellisdg <dgellis90@gmail.com>
2019-08-02 13:32:20 -0500

    FIX: sets subject_id

--- scripts/make_average_surface.py
+++ scripts/make_average_surface.py
@@ -25,10 +25,11 @@ def main(args):
         subject_ids = config[subset]
 
     for surface_hemisphere in config["hemispheres"]:
-        surface_basename = config["surface_basename_template"].format(hemi=surface_hemisphere).replace("pial",
-                                                                                                       "midthickness")
+        surface_basename = config["surface_basename_template"].format(hemi=surface_hemisphere,
+                                                                      subject_id="{subject_id}").replace("pial",
+                                                                                                         "midthickness")
         output_filename = os.path.join(output_directory,
-                                       output_basename + os.path.basename(surface_basename.format(subject=subset)))
+                                       output_basename + os.path.basename(surface_basename.format(subject_id=subset)))
         cmd = ["wb_command", "-surface-average", output_filename]
         for subject_id in subject_ids:
             subject_id = str(subject_id)
@@ -37,7 +38,7 @@ def main(args):
                                     subject_id,
                                     "MNINonLinear",
                                     "fsaverage_LR32k",
-                                    surface_basename.format(subject=subject_id)))
+                                    surface_basename.format(subject_id=subject_id)))
         print(" ".join(cmd))
         subprocess.call(cmd)
 

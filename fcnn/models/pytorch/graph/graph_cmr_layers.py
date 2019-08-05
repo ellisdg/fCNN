6e6e1588db093e1f1ab3df8e042c76cf9e265028
ellisdg <dgellis90@gmail.com>
2019-08-05 11:53:59 -0500

    debugging

--- fcnn/models/pytorch/graph/graph_cmr_layers.py
+++ fcnn/models/pytorch/graph/graph_cmr_layers.py
@@ -112,6 +112,8 @@ class SparseMM(torch.autograd.Function):
     """
     @staticmethod
     def forward(ctx, sparse, dense):
+        print(sparse.type())
+        print(dense.type())
         ctx.req_grad = dense.requires_grad
         ctx.save_for_backward(sparse)
         return torch.matmul(sparse, dense)

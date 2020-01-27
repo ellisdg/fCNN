840f164dfb5cded634ebe34b61d492fd51dfc9d7
David Ellis <david.ellis@unmc.edu>
2020-01-27 12:21:15 -0600

    FIX: don't iterate through string

--- scripts/make_average_fmri_cifti_volume.py
+++ scripts/make_average_fmri_cifti_volume.py
@@ -40,23 +40,32 @@ def make_average_cifti_volume(config, directory, output_directory, subset, outpu
     else:
         subject_ids = config[subset]
 
-    for target_basename in config["target_basenames"]:
-        output_filename = os.path.join(output_directory,
-                                       output_basename + os.path.basename(target_basename.format(subset)))
-        cifti_avg_cmd = ["wb_command", "-cifti-average", output_filename]
-        for subject_id in subject_ids:
-            subject_id = str(subject_id)
+    target_basenames = config["target_basenames"]
+    if type(target_basenames) == str:
+        target_basenames = [target_basenames]
+    for target_basename in target_basenames:
+        make_average_cifti_volume_for_target(target_basename, output_directory, output_basename, subset, subject_ids,
+                                             directory, reference_volume)
 
-            cifti_resample_cmd = ["wb_command", "-volume-warpfield-resample"]
-            moving_volume = os.path.join(directory, subject_id, target_basename.format(subject_id))
-            warpfield = os.path.join(directory, subject_id, "MNINonLinear", "xfms", "acpc_dc2standard.nii.gz")
-            output_volume = moving_volume.replace("T1w", "MNINonLinear").replace(".volume", "_resampled.volume")
-            cifti_resample_cmd.extend([moving_volume, warpfield, reference_volume, "TRILINEAR", output_volume,
-                                       "-fnirt", reference_volume])
-            run_command(cifti_resample_cmd)
-            cifti_avg_cmd.append("-cifti")
-            cifti_avg_cmd.append(output_volume)
-        run_command(cifti_avg_cmd)
+
+def make_average_cifti_volume_for_target(target_basename, output_directory, output_basename, subset, subject_ids,
+                                         directory, reference_volume):
+    output_filename = os.path.join(output_directory,
+                                   output_basename + os.path.basename(target_basename.format(subset)))
+    cifti_avg_cmd = ["wb_command", "-cifti-average", output_filename]
+    for subject_id in subject_ids:
+        subject_id = str(subject_id)
+
+        cifti_resample_cmd = ["wb_command", "-volume-warpfield-resample"]
+        moving_volume = os.path.join(directory, subject_id, target_basename.format(subject_id))
+        warpfield = os.path.join(directory, subject_id, "MNINonLinear", "xfms", "acpc_dc2standard.nii.gz")
+        output_volume = moving_volume.replace("T1w", "MNINonLinear").replace(".volume", "_resampled.volume")
+        cifti_resample_cmd.extend([moving_volume, warpfield, reference_volume, "TRILINEAR", output_volume,
+                                   "-fnirt", reference_volume])
+        run_command(cifti_resample_cmd)
+        cifti_avg_cmd.append("-cifti")
+        cifti_avg_cmd.append(output_volume)
+    run_command(cifti_avg_cmd)
 
 
 if __name__ == "__main__":

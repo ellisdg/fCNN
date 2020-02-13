2cb255483801dd643927f4ccdde13c49c8d7d7a4
David Ellis <david.ellis@unmc.edu>
2020-02-13 09:15:48 -0600

    ENH: flexible loading of model weights

--- fcnn/models/pytorch/build.py
+++ fcnn/models/pytorch/build.py
@@ -1,5 +1,5 @@
 import os
-
+import math
 import torch
 
 from .classification import resnet
@@ -35,14 +35,40 @@ def build_or_load_model(model_name, model_filename, n_features, n_outputs, n_gpu
     elif n_gpus > 0:
         model = model.cuda()
     if os.path.exists(model_filename):
-        try:
-            model.load_state_dict(torch.load(model_filename))
-        except RuntimeError as error:
-            if n_gpus > 1:
-                model.module.load_state_dict(torch.load(model_filename))
-            else:
-                raise error
+        state_dict = torch.load(model_filename)
+        model = load_state_dict(model, state_dict, n_gpus=n_gpus)
     return model
 
 
+def load_state_dict(model, state_dict, n_gpus, strict=False):
+    try:
+        if not strict:
+            state_dict = match_state_dict_shapes(model.state_dict(), state_dict)
+        model.load_state_dict(state_dict, strict=strict)
+    except RuntimeError as error:
+        if n_gpus > 1:
+            if not strict:
+                state_dict = match_state_dict_shapes(model.module.state_dict(), state_dict)
+            model.module.load_state_dict(state_dict, strict=strict)
+        else:
+            raise error
+    return model
 
+
+def match_state_dict_shapes(fixed_state_dict, moving_state_dict):
+    for key in fixed_state_dict:
+        if fixed_state_dict[key].size() != moving_state_dict[key].size():
+            moving_state_dict[key] = match_tensor_sizes(fixed_state_dict[key], moving_state_dict[key])
+    return moving_state_dict
+
+
+def match_tensor_sizes(fixed_tensor, moving_tensor):
+    fixed_tensor_size = fixed_tensor.size()
+    moving_tensor_size = moving_tensor.size()
+    for dim in range(len(moving_tensor_size)):
+        if fixed_tensor_size[dim] > moving_tensor_size[dim]:
+            moving_tensor = torch.cat([moving_tensor] *
+                                      int(math.ceil(fixed_tensor_size[dim] / moving_tensor_size[dim])))
+        if fixed_tensor_size[dim] != moving_tensor_size[dim]:
+            moving_tensor = moving_tensor.narrow(dim=dim, start=0, length=fixed_tensor_size[dim])
+    return moving_tensor

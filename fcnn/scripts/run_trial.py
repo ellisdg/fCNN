4bb15fd149d8e8ae466bdc0701e4660191c6a433
David Ellis <david.ellis@unmc.edu>
2020-02-25 17:53:58 -0600

    FIX: fix key

--- fcnn/scripts/run_trial.py
+++ fcnn/scripts/run_trial.py
@@ -86,7 +86,7 @@ def main():
     else:
         package = "keras"
 
-    if not config["n_outputs"] == len(config["metrics"]):
+    if not config["n_outputs"] == len(config["metric_names"]):
         raise ValueError("n_outputs set to {}, but number of metrics is {}.".format(config["n_outputs"],
                                                                                     config["metric_names"]))
 

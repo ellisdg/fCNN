b9055a883ed47bf59c466d7006d8372061696a4f
David Ellis <david.ellis@unmc.edu>
2020-03-12 14:53:48 -0500

    FIX

--- scripts/bash/run_sample_struct6_to_surface.sh
+++ scripts/bash/run_sample_struct6_to_surface.sh
@@ -5,5 +5,6 @@ conda activate dti
 export PYTHONPATH=/home/aizenberg/dgellis/fCNN:$PYTHONPATH
 export PATH=/work/aizenberg/dgellis/tools/workbench/bin_rh_linux64:$PATH
 SurfaceName=${1}
+fCNNDir=/home/aizenberg/dgellis/fCNN
 Method=enclosing
-${HOME}/.conda/envs/dti/bin/python scripts/sample_volume_to_surface.py /work/aizenberg/dgellis/HCP/HCP_1200 data/subjects_v4.json "T1w/fsaverage_LR32k/{subject}.{hemi}.$SurfaceName.32k_fs_LR.surf.gii" ${Method} T1w/struct6_normalized.nii.gz data/labels/STRUCT6_name-file.txt "MNINonLinear/fsaverage_LR32k/{subject}.{hemi}.atlasroi.32k_fs_LR.shape.gii" $SurfaceName
+${HOME}/.conda/envs/dti/bin/python scripts/sample_volume_to_surface.py /work/aizenberg/dgellis/HCP/HCP_1200 $fCNNDir/data/subjects_v4.json "T1w/fsaverage_LR32k/{subject}.{hemi}.$SurfaceName.32k_fs_LR.surf.gii" ${Method} $fCNNDir/T1w/struct6_normalized.nii.gz $fCNNDir/data/labels/STRUCT6_name-file.txt "MNINonLinear/fsaverage_LR32k/{subject}.{hemi}.atlasroi.32k_fs_LR.shape.gii" $SurfaceName

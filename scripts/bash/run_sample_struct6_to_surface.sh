105208197ebebaa1aa30b85003633a71c895f30e
David Ellis <david.ellis@unmc.edu>
2020-03-12 14:13:00 -0500

    FIX

--- scripts/bash/run_sample_struct6_to_surface.sh
+++ scripts/bash/run_sample_struct6_to_surface.sh
@@ -5,4 +5,4 @@ conda activate dti
 export PYTHONPATH=/home/aizenberg/dgellis/fCNN:$PYTHONPATH
 SurfaceName=${1}
 Method=enclosing
-${HOME}/.conda/envs/dti/bin/python scripts/sample_volume_to_surface.py /work/aizenberg/dgellis/HCP/HCP_1200 data/subjects_v4.json "T1w/fsaverage_LR32k/{subject}.{hemi}.$SurfaceName.32k_fs_LR.gii" ${Method} T1w/struct6_nomralized.nii.gz data/labels/STRUCT6_name-file.txt "MNINonLinear/fsaverage_LR32k/{subject}.{hemi}.atlasroi.32k_fs_LR.shape.gii" $SurfaceName
+${HOME}/.conda/envs/dti/bin/python scripts/sample_volume_to_surface.py /work/aizenberg/dgellis/HCP/HCP_1200 data/subjects_v4.json "T1w/fsaverage_LR32k/{subject}.{hemi}.$SurfaceName.32k_fs_LR.gii" ${Method} T1w/struct6_normalized.nii.gz data/labels/STRUCT6_name-file.txt "MNINonLinear/fsaverage_LR32k/{subject}.{hemi}.atlasroi.32k_fs_LR.shape.gii" $SurfaceName

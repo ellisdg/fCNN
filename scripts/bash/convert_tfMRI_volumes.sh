6fac70c87ed3892ddfc19d391cabb5d7f2206bd4
ellisdg <dgellis90@gmail.com>
2020-03-24 08:56:45 -0500

    ENH: can submit script via slurm

--- scripts/bash/convert_tfMRI_volumes.sh
+++ scripts/bash/convert_tfMRI_volumes.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+#SBATCH --time=48:00:00          # Run time in hh:mm:ss
+#SBATCH --job-name=vol_convert
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=4000       # Maximum memory required per CPU (in megabytes)
+#SBATCH --error=/work/aizenberg/dgellis/fCNN/logs/job.%J.err
+#SBATCH --output=/work/aizenberg/dgellis/fCNN/logs/job.%J.out
 module load anaconda
 conda activate dti
 export PYTHONPATH=/home/aizenberg/dgellis/fCNN:$PYTHONPATH

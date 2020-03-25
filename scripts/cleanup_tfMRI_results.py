cf74d7b5bfb3f45350d4126c3bf6ef7f6603a7c0
ellisdg <dgellis90@gmail.com>
2020-03-25 15:37:38 -0500

    FIX

--- scripts/cleanup_tfMRI_results.py
+++ scripts/cleanup_tfMRI_results.py
@@ -10,6 +10,7 @@ def run_command(cmd):
 
 
 def main():
+    overwrite = True
     for folder in glob.glob("/work/aizenberg/dgellis/HCP/HCP_1200/*/T1w/Results/tfMRI*/tfMRI*s2_level1.feat/StandardVolumeStats"):
         feat_dir = os.path.dirname(folder)
         basename = os.path.basename(feat_dir).replace("_level1.feat", "").replace("_hp", "_{}_hp")
@@ -17,16 +18,17 @@ def main():
         for name in ("cope", "zstat"):
             output_filename = os.path.join(feat_dir, basename.format(name))
             cmd = ["wb_command", "-volume-merge", output_filename]
-            if not os.path.exists(output_filename):
+            if overwrite or not os.path.exists(output_filename):
                 _files = glob.glob(os.path.join(folder, "{}*.nii.gz".format(name)))
                 for i in range(len(_files)):
-                    _file = os.path.join(folder, "{}{}.nii.gz".format(name, i))
+                    _file = os.path.join(folder, "{}{}.nii.gz".format(name, i+1))
                     cmd.extend(["-volume", _file])
             run_command(cmd)
             if not os.path.exists(output_filename):
                 success = False
         if success:
-            shutil.rmtree(folder)
+            # shutil.rmtree(folder)
+            pass
 
 
 if __name__ == "__main__":

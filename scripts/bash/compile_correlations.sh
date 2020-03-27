cb691b12987b067c6719c367ff71fd0cd40de8a2
ellisdg <dgellis90@gmail.com>
2020-03-27 08:43:27 -0500

    ENH

--- scripts/bash/compile_correlations.sh
+++ scripts/bash/compile_correlations.sh
@@ -6,5 +6,6 @@
 #SBATCH --error=/work/aizenberg/dgellis/fCNN/logs/job.%J.err
 #SBATCH --output=/work/aizenberg/dgellis/fCNN/logs/job.%J.out
 TASK=${1}
+fCNNDir=/home/aizenberg/dgellis/fCNN
 PDIR=/work/aizenberg/dgellis/fCNN/predictions/v4_struct6_unet_${TASK}_2mm_v1_pt
-python fCNN/scripts/compile_predicted_v_actual_correlations.py ${PDIR}/correlations.npy /home/aizenberg/dgellis/fCNN/data/v4_struct6_unet_${TASK}-TAVOR_2mm_v1_pt_config.json ${PDIR} /home/aizenberg/dgellis/fCNN/data/labels/${TASK}-TAVOR_name-file.txt
+python $fCNNDir/scripts/compile_predicted_v_actual_correlations.py ${PDIR}/correlations.npy $fCNNDir/data/v4_struct6_unet_${TASK}-TAVOR_2mm_v1_pt_config.json ${PDIR} $fCNNDir/data/labels/${TASK}-TAVOR_name-file.txt

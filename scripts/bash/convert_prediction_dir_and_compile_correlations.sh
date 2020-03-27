a7c232a83de55ed4bbf86ea54e76730590503055
ellisdg <dgellis90@gmail.com>
2020-03-27 08:39:01 -0500

    ENH

---
+++ scripts/bash/convert_prediction_dir_and_compile_correlations.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --time=7-00:00:00          # Run time in hh:mm:ss
+#SBATCH --job-name=correlations
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=4000       # Maximum memory required per CPU (in megabytes)
+#SBATCH --error=/work/aizenberg/dgellis/fCNN/logs/job.%J.err
+#SBATCH --output=/work/aizenberg/dgellis/fCNN/logs/job.%J.out
+TASK=${1}
+bash /home/aizenberg/dgellis/fCNN/scripts/bash/convert_tfMRI_prediction_dir.sh ${TASK}
+bash /home/aizenberg/dgellis/fCNN/scripts/bash/compile_correlations.sh ${TASK}
--- scripts/bash/convert_tfMRI_prediction_dir.sh
+++ scripts/bash/convert_tfMRI_prediction_dir.sh
@@ -7,6 +7,6 @@ export PATH=/work/aizenberg/dgellis/tools/workbench/bin_rh_linux64:$PATH
 SurfaceName=midthickness
 fCNNDir=/home/aizenberg/dgellis/fCNN
 Method="ribbon-constrained"
-TASK=SOCIAL
+TASK=${1}
 PredDir=/work/aizenberg/dgellis/fCNN/predictions/v4_struct6_unet_${Task}_2mm_v1_pt
 ${HOME}/.conda/envs/dti/bin/python scripts/sample_volume_to_surface.py /work/aizenberg/dgellis/HCP/HCP_1200 $fCNNDir/data/subjects_v4.json "T1w/fsaverage_LR32k/{subject}.{hemi}.$SurfaceName.32k_fs_LR.surf.gii" ${Method} "${PredDir}/{subject}_model_v4_struct6_unet_${TASK}_2mm_v1_pt_struct6_normalized.nii.gz" $fCNNDir/data/labels/${TASK}-TAVOR_name-file.txt "MNINonLinear/fsaverage_LR32k/{subject}.{hemi}.atlasroi.32k_fs_LR.shape.gii" $SurfaceName

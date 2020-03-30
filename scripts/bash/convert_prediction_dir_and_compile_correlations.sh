803ce1197928e7e60614f1e8a9bf875368bce77b
ellisdg <dgellis90@gmail.com>
2020-03-30 09:42:06 -0500

    FIX

--- scripts/bash/convert_prediction_dir_and_compile_correlations.sh
+++ scripts/bash/convert_prediction_dir_and_compile_correlations.sh
@@ -5,6 +5,7 @@
 #SBATCH --mem-per-cpu=4000       # Maximum memory required per CPU (in megabytes)
 #SBATCH --error=/work/aizenberg/dgellis/fCNN/logs/job.%J.err
 #SBATCH --output=/work/aizenberg/dgellis/fCNN/logs/job.%J.out
+export PYTHONPATH=/home/aizenberg/dgellis/fCNN:/home/aizenberg/dgellis/3DUnetCNN:PYTHONPATH
 TASK=${1}
-bash /home/aizenberg/dgellis/fCNN/scripts/bash/convert_tfMRI_prediction_dir.sh ${TASK}
+# bash /home/aizenberg/dgellis/fCNN/scripts/bash/convert_tfMRI_prediction_dir.sh ${TASK}
 bash /home/aizenberg/dgellis/fCNN/scripts/bash/compile_correlations.sh ${TASK}

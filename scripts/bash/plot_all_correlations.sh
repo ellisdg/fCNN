6d44f883c92d338e833a841846cfb47afdb34a86
ellisdg <dgellis90@gmail.com>
2020-03-30 10:00:32 -0500

    Remove RELATIONAL

--- scripts/bash/plot_all_correlations.sh
+++ scripts/bash/plot_all_correlations.sh
@@ -3,11 +3,9 @@ python /home/aizenberg/dgellis/fCNN/scripts/plot_combined_correlations.py\
  /work/aizenberg/dgellis/fCNN/predictions/v4_struct6_unet_MOTOR-TAVOR_2mm_v1_pt/correlations.npy,\
 /work/aizenberg/dgellis/fCNN/predictions/v4_struct6_unet_LANGUAGE_2mm_v1_pt/correlations.npy,\
 /work/aizenberg/dgellis/fCNN/predictions/v4_struct6_unet_WM_2mm_v1_pt/correlations.npy,\
-/work/aizenberg/dgellis/fCNN/predictions/v4_struct6_unet_SOCIAL_2mm_v1_pt/correlations.npy,\
-/work/aizenberg/dgellis/fCNN/predictions/v4_struct6_unet_EMOTION_2mm_v1_pt/correlations.npy\
+/work/aizenberg/dgellis/fCNN/predictions/v4_struct6_unet_SOCIAL_2mm_v1_pt/correlations.npy\
  /home/aizenberg/dgellis/fCNN/data/labels/MOTOR-TAVOR_name-file.txt,\
 /home/aizenberg/dgellis/fCNN/data/labels/LANGUAGE-TAVOR_name-file.txt,\
 /home/aizenberg/dgellis/fCNN/data/labels/WM-TAVOR_name-file.txt,\
-/home/aizenberg/dgellis/fCNN/data/labels/SOCIAL-TAVOR_name-file.txt,\
-/home/aizenberg/dgellis/fCNN/data/labels/EMOTION-TAVOR_name-file.txt\
+/home/aizenberg/dgellis/fCNN/data/labels/SOCIAL-TAVOR_name-file.txt\
  /work/aizenberg/dgellis/fCNN/predictions/figures

71c46dc6cd993c6b1494bee99d8209d8175d9949
ellisdg <dgellis90@gmail.com>
2020-04-14 18:14:04 -0500

    FIX

--- scripts/plot_compared_correlation_models.py
+++ scripts/plot_compared_correlation_models.py
@@ -85,8 +85,7 @@ def main():
         extra_diag_values = corr_matrix[diagonal_mask == False]
         result.append((diag_values.mean() - extra_diag_values.mean())/extra_diag_values.mean())
         titles.append(title)
-    data = np.dstack([method_labels, tasks, names, titles, np.asarray(result) * 100])
-    print(data.shape)
+    data = np.squeeze(np.dstack([method_labels, tasks, names, titles, np.asarray(result) * 100]))
     df = pd.DataFrame(data, columns=["Method", "Domain", "Contrast", "Task", "Value"])
     w = 6
     width = 0.4

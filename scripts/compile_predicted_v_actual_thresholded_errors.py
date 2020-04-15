4fe5ef08020373f05a1f810a56282bceaf9607b0
ellisdg <dgellis90@gmail.com>
2020-04-15 16:57:10 -0500

    ENH: multiprocessing

--- scripts/compile_predicted_v_actual_thresholded_errors.py
+++ scripts/compile_predicted_v_actual_thresholded_errors.py
@@ -20,7 +20,8 @@ def read_namefile(filename):
     return names
 
 
-def compute_errors(predicted_fn, target_fn, group_average_data_thresholded, metric_names, structure_names, verbose=False):
+def compute_errors(predicted_fn, target_fn, group_average_data_thresholded, metric_names, structure_names,
+                   verbose=False):
     if verbose:
         print(predicted_fn)
     predicted_image = nib.load(predicted_fn)
@@ -39,6 +40,21 @@ def compute_errors(predicted_fn, target_fn, group_average_data_thresholded, metr
     return prediction_errors + group_average_errors
 
 
+def mp_compute_errors(inputs, group_average_fn, metric_names, structure_names, verbose=False):
+    group_average_thresholded_data = read_and_threshold_image_data(group_average_fn, metric_names, structure_names)
+    return compute_errors(inputs[0], inputs[1], group_average_thresholded_data, metric_names, structure_names,
+                          verbose=verbose)
+
+
+def read_and_threshold_image_data(group_average_fn, metric_names, structure_names):
+    group_average_image = nib.load(group_average_fn)
+    group_average_data = get_metric_data([group_average_image], metric_names=[metric_names],
+                                         surface_names=structure_names, subject_id=None)
+    group_average_thresholded_data = threshold_data_for_all_metrics(group_average_data,
+                                                                    metric_names=metric_names)
+    return group_average_thresholded_data
+
+
 def compute_mae(data1, data2):
     return np.mean(np.abs(data1 - data2))
 
@@ -48,17 +64,13 @@ def compute_mse(data1, data2):
 
 
 def g2gm_threshold(data, iterations=1000):
-    print(data.shape)
     model = GGGM()
     membership = model.estimate(data, niter=iterations)
     lower_threshold = quantile_1D(data, membership[..., 0], 0.5)
-    print(lower_threshold)
     upper_threshold = quantile_1D(data, membership[..., 2], 0.5)
-    print(upper_threshold)
     thresholded_data = np.zeros((2,) + data.shape, np.int)
     thresholded_data[0][data >= upper_threshold] = 1
     thresholded_data[1][data <= lower_threshold] = 1
-    print(np.sum(thresholded_data[0]), np.sum(thresholded_data[1]))
     return thresholded_data
 
 
@@ -81,6 +93,9 @@ def main():
     name_file = sys.argv[4]
     group_average_filename = sys.argv[5]
 
+    verbose = True
+    pool_size = 32
+
     target_images = list()
     prediction_images = list()
 
@@ -93,13 +108,6 @@ def main():
     metric_names = read_namefile(name_file)
     corrected_metric_names = [m.split(" ")[-1] for m in metric_names]
 
-    group_average_image = nib.load(group_average_filename)
-    group_average_data = get_metric_data([group_average_image], metric_names=[corrected_metric_names],
-                                         surface_names=structure_names, subject_id=None)
-    group_average_thresholded_data = threshold_data_for_all_metrics(group_average_data,
-                                                                    metric_names=corrected_metric_names)
-
-    pool_size = None
     subjects = list()
     for p_image_fn in all_prediction_images:
         if "target" not in p_image_fn:
@@ -112,17 +120,20 @@ def main():
             prediction_images.append(p_image_fn)
     errors = list()
     if pool_size is not None:
-        func = partial(compute_errors, metric_names=corrected_metric_names, structure_names=structure_names,
-                       verbose=True)
+        func = partial(mp_compute_errors, metric_names=corrected_metric_names, structure_naames=structure_names,
+                       verbose=verbose, group_average_fn=group_average_filename)
         pool = Pool(pool_size)
-        errors = pool.map(func, prediction_images)
+        errors = pool.map(func, zip(prediction_images, target_images))
     else:
+        group_average_thresholded_data = read_and_threshold_image_data(group_average_filename, corrected_metric_names,
+                                                                       structure_names)
         for i, (p_image_fn, t_image_fn) in enumerate(zip(prediction_images, target_images)):
             update_progress(i/len(prediction_images), message=os.path.basename(p_image_fn).split("_")[0])
             errors.append(compute_errors(predicted_fn=p_image_fn, target_fn=t_image_fn,
                                          metric_names=corrected_metric_names,
                                          structure_names=structure_names,
-                                         group_average_data_thresholded=group_average_thresholded_data))
+                                         group_average_data_thresholded=group_average_thresholded_data,
+                                         verbose=verbose))
         update_progress(1)
     df = pd.DataFrame(errors, index=subjects, columns=metric_names + ["Group Average " + m for m in metric_names])
     df.to_csv(output_file)

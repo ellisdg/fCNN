3f192334e58731743d8e16009dc17ddaf0257915
ellisdg <dgellis90@gmail.com>
2020-04-28 18:22:20 -0500

    ENH: fancies up the lateralization index plots.

--- scripts/plot_correlations.py
+++ scripts/plot_correlations.py
@@ -87,11 +87,18 @@ def main():
         lateralization = np.load(lateralization_file)
 
     for task_ind in range(lateralization.shape[-1]):
-        fig, ax = plt.subplots(figsize=(18, 6))
+        fig_width = 18
+        fig_height = 6
+        fig, ax = plt.subplots(figsize=(fig_width, fig_height))
         x = np.arange(lateralization.shape[0])
         ind = np.argsort(lateralization[..., 1, task_ind], axis=0)
         ax.bar(x=x, height=lateralization[..., 1, task_ind][ind], width=0.5, label="actual")
         ax.bar(x=x + 0.5, height=lateralization[..., 0, task_ind][ind], width=0.5, label="predicted")
+        ax.set_xticks(x[::int(len(x)/(fig_width * 0.5))])
+        seaborn.despine(ax=ax, top=True, left=False, bottom=False, right=True)
+        ax.axhline(0, color="black", linewidth=1)
+        ax.set_xlabel("Subjects")
+        ax.set_ylabel(r"RIGHT $\leftarrow$ Lateralization Index $\rightarrow$ LEFT")
         ax.legend()
         ax.set_title(metric_names[task_ind])
         fig.savefig(output_dir + '/lateralization_{task}_{name}.png'.format(task=task, name=metric_names[task_ind]))

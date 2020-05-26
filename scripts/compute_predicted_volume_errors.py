e213348b904adf0bb5a2560f5ae084226fb238bb
ellisdg <dgellis90@gmail.com>
2020-05-26 10:05:49 -0500

    ENH: volumetric correlation analysis

--- scripts/compute_predicted_volume_errors.py
+++ scripts/compute_predicted_volume_errors.py
@@ -6,6 +6,9 @@ import glob
 import numpy as np
 import nibabel as nib
 import pandas as pd
+from multiprocessing import Pool
+from functools import partial
+from scipy.stats import pearsonr
 
 
 def load_json(filename):
@@ -18,22 +21,36 @@ def main(args):
     prediction_directory = os.path.abspath(args[1])
     directory = os.path.abspath(args[2])
     config = load_json(args[3])
+    n_threads = int(args[4])
     dir_name = "MNINonLinear"
     overwrite = False
     transformed = transform_prediction_dir(prediction_dir=prediction_directory, directory=directory,
                                            reference_volume=reference_volume, dir_name=dir_name, overwrite=overwrite,
                                            target_basename=config["target_basenames"])
     output_filename = os.path.join(prediction_directory, dir_name, dir_name + "errors.csv")
-    compute_errors(transformed, output_filename=output_filename, columns=config["metric_names"])
+    compute_errors(transformed, output_filename=output_filename, columns=config["metric_names"], pool_size=n_threads)
+    compute_correlations(transformed, output_file=os.path.join(prediction_directory, dir_name, dir_name +
+                                                               "correlations.npy"),
+                         metric_names=config["metric_names"], pool_size=n_threads, verbose=True)
 
 
-def compute_errors(transformed, output_filename, columns):
-    errors = list()
-    index = list()
-    for subject_id, filename, target_filename in transformed:
-        mae = compute_mean_absolute_error(filename, target_filename)
-        errors.append(mae)
-        index.append(subject_id)
+def compute_errors(transformed, output_filename, columns, pool_size=1, axis=(0, 1, 2)):
+    if pool_size > 1:
+        index = list()
+        filenames = list()
+        for subject_id, filename, target_filename in transformed:
+            filenames.append((filename, target_filename))
+            index.append(subject_id)
+        pool = Pool(pool_size)
+        func = partial(mp_compute_meant_absolute_error, axis=axis)
+        errors = pool.map(func, filenames)
+    else:
+        errors = list()
+        index = list()
+        for subject_id, filename, target_filename in transformed:
+            mae = compute_mean_absolute_error(filename, target_filename, axis=axis)
+            errors.append(mae)
+            index.append(subject_id)
     df = pd.DataFrame(errors, index=index, columns=columns)
     df.to_csv(output_filename)
 
@@ -45,6 +62,10 @@ def compute_mean_absolute_error(filename1, filename2, axis=(0, 1, 2)):
     np.testing.assert_equal(image1.shape, image2.shape)
     error = image1.get_fdata() - image2.get_fdata()
     return np.mean(np.abs(error), axis=axis)
+
+
+def mp_compute_meant_absolute_error(filenames, axis=(0, 1, 2)):
+    return compute_mean_absolute_error(filenames[0], filenames[1], axis=axis)
 
 
 def run_command(cmd):
@@ -59,22 +80,35 @@ def transform_prediction_dir(prediction_dir, directory, reference_volume, target
         os.makedirs(output_dir)
     outputs = list()
     for prediction_volume in glob.glob(os.path.join(prediction_dir, "*.nii.gz")):
-        basename = os.path.basename(prediction_volume)
-        if "target" not in basename:
-            subject_id = basename.split("_")[0]
-            print(subject_id)
-            warpfield = os.path.join(directory, subject_id, "MNINonLinear", "xfms", "acpc_dc2standard.nii.gz")
-            output_filename = os.path.join(output_dir, basename)
-            transformed_volume = transform_volume(prediction_volume, warpfield, reference_volume,
-                                                  output_volume=output_filename, overwrite=overwrite)
-            target_filename = os.path.join(directory, subject_id,
-                                           target_basename.format(subject_id))
-            transformed_target_filename = target_filename.replace("T1w", dir_name).replace(".volume.dscalar",
-                                                                                           "_resampled")
-            transform_volume(target_filename, warpfield, reference_volume, transformed_target_filename,
-                             overwrite=overwrite)
-            outputs.append((subject_id, transformed_volume, transformed_target_filename))
+        _outputs = transform_prediction_target_volumes(prediction_volume=prediction_volume, hcp_directory=directory,
+                                                       output_dir=output_dir,
+                                                       reference_volume_filename=reference_volume,
+                                                       target_basename_template=target_basename, overwrite=overwrite)
+        if _outputs is not None:
+            outputs.append(_outputs)
     return outputs
+
+
+def transform_prediction_target_volumes(prediction_volume, hcp_directory, output_dir, reference_volume_filename,
+                                        target_basename_template, overwrite=False):
+    basename = os.path.basename(prediction_volume)
+    if "target" not in basename and "dscalar" not in basename:
+        subject_id = basename.split("_")[0]
+        print(subject_id)
+        warpfield = os.path.join(hcp_directory, subject_id, "MNINonLinear", "xfms", "acpc_dc2standard.nii.gz")
+        output_filename = os.path.join(output_dir, basename)
+        transformed_volume_filename = transform_volume(prediction_volume, warpfield, reference_volume_filename,
+                                                       output_volume=output_filename, overwrite=overwrite)
+        target_filename = os.path.join(hcp_directory, subject_id,
+                                       target_basename_template.format(subject_id))
+        transformed_target_filename = target_filename.replace("T1w/", "MNINonLinear/").replace(".nii",
+                                                                                               "_resampled.nii")
+        target_dirname = os.path.dirname(transformed_target_filename)
+        if not os.path.exists(target_dirname):
+            os.makedirs(target_dirname)
+        transform_volume(target_filename, warpfield, reference_volume_filename, transformed_target_filename,
+                         overwrite=overwrite)
+        return subject_id, transformed_volume_filename, transformed_target_filename
 
 
 def transform_volume(nifti_volume, warpfield, reference_volume, output_volume, overwrite=False):
@@ -83,6 +117,46 @@ def transform_volume(nifti_volume, warpfield, reference_volume, output_volume, o
     if overwrite or not os.path.exists(output_volume):
         run_command(resample_cmd)
     return output_volume
+
+
+def compute_correlations(inputs, output_file, metric_names, pool_size=1, verbose=True):
+    subjects = list()
+    prediction_images = list()
+    target_images = list()
+    for subject_id, filename, target_filename in inputs:
+        prediction_images.append(filename)
+        target_images.append(target_filename)
+        subjects.append(subject_id)
+    correlations = list()
+    if pool_size is not None:
+        func = partial(compute_correlation_row, target_fns=target_images, metric_names=metric_names, verbose=verbose)
+        pool = Pool(pool_size)
+        correlations = pool.map(func, prediction_images)
+    else:
+        for i, p_image_fn in enumerate(prediction_images):
+            correlations.append(compute_correlation_row(p_image_fn, target_images, metric_names, verbose=verbose))
+    np.save(output_file, correlations)
+    np.save(output_file.replace(".npy", "_subjects.npy"), subjects)
+
+
+def compute_correlation_row(predicted_fn, target_fns, metric_names, verbose=False):
+    if verbose:
+        print(predicted_fn)
+    predicted_image = nib.load(predicted_fn)
+    predicted_data = predicted_image.get_fdata()
+    row = list()
+    for fn in target_fns:
+        row.append(compute_correlation(target_fn=fn, predicted_data=predicted_data, metric_names=metric_names))
+    return row
+
+
+def compute_correlation(target_fn, predicted_data, metric_names):
+    target_image = nib.load(target_fn)
+    target_data = target_image.get_fdata()
+    task_row = list()
+    for i, task_name in enumerate(metric_names):
+        task_row.append(pearsonr(predicted_data[..., i].flatten(), target_data[..., i].flatten()))
+    return task_row
 
 
 if __name__ == "__main__":

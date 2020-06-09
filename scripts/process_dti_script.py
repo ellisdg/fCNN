d0b100a040bdd1d885bd6bb2dce907be0bd6d6f6
ellisdg <dgellis90@gmail.com>
2020-06-09 16:43:44 -0500

    ENH: process dti script

--- scripts/process_dti_script.py
+++ scripts/process_dti_script.py
@@ -1,16 +1,34 @@
 import sys
 import os
+import argparse
+from multiprocessing import Pool
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
-from fcnn.dti import process_dti
+from fcnn.dti import process_dti, process_multi_b_value_dti
+from fcnn.utils.utils import load_json
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--subjects_filename')
+    parser.add_argument('--group')
+    parser.add_argument('--hcp-dir')
+    parser.add_argument('--multi-b-value', action='store_true', default=False)
+    parser.add_argument('--nthreads', type=int, default=1)
+    return vars(parser.parse_args())
 
 
 def main():
-    subjects = sys.argv[1].split(",")
-    hcp_dir = os.path.abspath(sys.argv[2])
-    for subject in subjects:
-        print("Processing :", subject)
-        subject_dir = os.path.join(hcp_dir, subject)
-        process_dti(subject_dir)
+    args = parse_args()
+    group = args['group']
+    subjects = load_json(args["subjects_filename"])[group]
+    hcp_dir = args["hcp-dir"]
+    subject_dirs = [os.path.join(hcp_dir, subject) for subject in subjects]
+    pool = Pool(args['n_threads'])
+    if args['multi-b-value']:
+        process_func = process_multi_b_value_dti
+    else:
+        process_func = process_dti
+    pool.map(process_func, subject_dirs)
 
 
 if __name__ == '__main__':

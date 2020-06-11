8d2f4ee334e5bf43e5fd8f944c28b0e33f571f7d
ellisdg <dgellis90@gmail.com>
2020-06-11 17:08:43 -0500

    FIX

--- scripts/bash/convert_tfMRI_volumes_retest.sh
+++ scripts/bash/convert_tfMRI_volumes_retest.sh
@@ -25,7 +25,7 @@ do
     ${HOME}/.conda/envs/dti/bin/python ${fCNNDir}/scripts/convert_cifti_volumes.py data/v4_struct6_unet_${Task}_2mm_v1_pt_config.json ${Dir} ${Basename} ${SubjectsConfig}
   done
   # combine the nifti volumes
-  ${HOME}/.conda/envs/dti/bin/python ${fCNNDir}/scripts/convert_cifti_volumes ${SubjectsConfig} ${Dir}
+  ${HOME}/.conda/envs/dti/bin/python ${fCNNDir}/scripts/combine_tfMRI_volumes.py ${SubjectsConfig} ${Dir}
   # sample the nifti volumes to the surface
   ${HOME}/.conda/envs/dti/bin/python ${fCNNDir}/scripts/sample_volume_to_surface.py ${Dir} ${SubjectsConfig} "T1w/fsaverage_LR32k/{subject}.{hemi}.$SurfaceName.32k_fs_LR.surf.gii" ${Method} "T1w/Results/tfMRI_ALL/tfMRI_ALL_hp200_s2_level2.feat/{subject}_tfMRI_ALL_level2_zstat_hp200_s2_${Basename}.nii.gz" $fCNNDir/data/labels/ALL-TAVOR_name-file.txt "MNINonLinear/fsaverage_LR32k/{subject}.{hemi}.atlasroi.32k_fs_LR.shape.gii" $SurfaceName
 done

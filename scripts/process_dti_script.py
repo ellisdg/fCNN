bc1215d8f3ca68a8d5e54761a8ce6188b8832237
David Ellis <david.ellis@unmc.edu>
2020-06-15 14:11:52 -0500

    FIX: fixes the slurm submission

--- scripts/process_dti_script.py
+++ scripts/process_dti_script.py
@@ -3,9 +3,13 @@ import os
 import argparse
 import subprocess
 from multiprocessing import Pool
+import json
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
-from fcnn.dti import process_dti, process_multi_b_value_dti
-from fcnn.utils.utils import load_json
+
+
+def load_json(filename):
+    with open(filename) as opened_file:
+        return json.load(opened_file)
 
 
 def parse_args():
@@ -21,24 +25,23 @@ def parse_args():
 
 
 def submit(subject_dirs, python="/home/aizenberg/dgellis/.conda/envs/dti/bin/python", nthreads=1, mem_per_cpu=8000,
-           flags="", delete_script=True):
+           flags="", delete_script=False):
 
     for subject_dir in subject_dirs:
         slurm_script_filename = os.path.join(".", "DTI_{}.slurm".format(os.path.basename(subject_dir)))
-        sbatch = """
-        #!/bin/bash
-        #SBATCH --time=7-00:00:00          # Run time in hh:mm:ss
-        #SBATCH --mem_per_cpu-per-cpu={}       # Maximum memory required per CPU (in megabytes)
-        #SBATCH --ntasks-per-node={}
-        #SBATCH --job-name=DTI_{}
-        #SBATCH --error=/work/aizenberg/dgellis/fCNN/logs/job.DTI_%J.err
-        #SBATCH --output=/work/aizenberg/dgellis/fCNN/logs/job.DTI_%J.err
-        conda activate dti
-        export PYTHONPATH=/home/aizenberg/dgellis/fCNN:$PYTHONPATH
-        {} {} --subject_dir {}{}
-        """.format(
-            nthreads, mem_per_cpu, os.path.basename(subject_dir), python, __file__, subject_dir, flags)
-        with open(slurm_script_filename) as temp:
+        sbatch = "#!/bin/bash\n" \
+                 "#SBATCH --time=7-00:00:00          # Run time in hh:mm:ss\n" \
+                 "#SBATCH --mem-per-cpu={}       # Maximum memory required per CPU (in megabytes)\n" \
+                 "#SBATCH --ntasks-per-node={}\n" \
+                 "#SBATCH --job-name=DTI_{}\n" \
+                 "#SBATCH --error=/work/aizenberg/dgellis/fCNN/logs/job.DTI_%J.err\n" \
+                 "#SBATCH --output=/work/aizenberg/dgellis/fCNN/logs/job.DTI_%J.err\n" \
+                 "module load anaconda\n" \
+                 "source /home/aizenberg/dgellis/.conda/envs/dti/bin/activate dti\n" \
+                 "export PYTHONPATH=/home/aizenberg/dgellis/fCNN:/home/aizenberg/dgellis/3DUnetCNN/:$PYTHONPATH\n" \
+                 "{} {} --subject_dir {}{}\n".format(
+                     mem_per_cpu, nthreads, os.path.basename(subject_dir), python, __file__, subject_dir, flags)
+        with open(slurm_script_filename, "w") as temp:
             temp.write(sbatch)
         cmd = ['sbatch', slurm_script_filename]
         subprocess.call(cmd)
@@ -61,6 +64,7 @@ def main():
             flags += " --multi_b_value"
         submit(subject_dirs, flags=flags, nthreads=args['nthreads'])
     else:
+        from fcnn.dti import process_dti, process_multi_b_value_dti
         if args['multi_b_value']:
             process_func = process_multi_b_value_dti
         else:

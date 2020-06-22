57501b24a6a26a41dbd23b7aab6b03db00e129d8
ellisdg <dgellis90@gmail.com>
2020-06-22 14:30:25 -0500

    ENH: adds argument parsing

--- scripts/sample_volume_to_surface.py
+++ scripts/sample_volume_to_surface.py
@@ -1,7 +1,7 @@
-import sys
 import subprocess
 import os
 import json
+import argparse
 from fcnn.utils.utils import update_progress
 
 
@@ -47,25 +47,35 @@ def download(f1, f2, include=None, exclude=None, verbose=False):
     run_command(cmd, verbose=verbose)
 
 
-def main():
-    directory = sys.argv[1]
-    config_filename = sys.argv[2]
-    surface_template = sys.argv[3]
-    method = sys.argv[4]
-    volume_template = sys.argv[5]
-    name_file = sys.argv[6]
-    atlas_roi_template = sys.argv[7]
-    surface_name = sys.argv[8]
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--volume_template', required=True)
+    parser.add_argument('--config', default="/home/aizenberg/dgellis/fCNN/data/subjects_v4-retest.json")
+    parser.add_argument('--hcp_dir', default="/work/aizenberg/dgellis/HCP/HCP_1200")
+    parser.add_argument('--surface_name', default="midthickness")
+    parser.add_argument('--method', default="ribbon-constrained")
+    parser.add_argument('--surface_template',
+                        default="T1w/fsaverage_LR32k/{subject}.{hemi}.{surface_name}.32k_fs_LR.surf.gii")
+    parser.add_argument('--atlas_roi_template',
+                        default="T1w/fsaverage_LR32k/{subject}.{hemi}.{surface_name}.32k_fs_LR.surf.gii")
+    parser.add_argument('--task_names', default="/home/aizenberg/dgellis/fCNN/data/labels/ALL-TAVOR_name-file.txt")
+    parser.add_argument('--overwrite', action="store_true", default=False)
+    parser.add_argument('--verbose', action="store_true", default=False)
+    return vars(parser.parse_args())
 
-    atlas_roi_template = os.path.join(directory, "{subject}", atlas_roi_template)
+
+def main():
+    args = parse_args()
+    atlas_roi_template = os.path.join(args["hcp_dir"], "{subject}", args["atlas_roi_template"])
+    surface_template = os.path.join(args["hcp_dir"], "{subject}", args["surface_template"])
+    volume_template = args["volume_template"]
     if not os.path.isabs(volume_template):
-        volume_template = os.path.join(directory, "{subject}", volume_template)
-    surface_template = os.path.join(directory, "{subject}", surface_template)
+        volume_template = os.path.join(args["hcp_dir"], "{subject}", volume_template)
     subject_ids = list()
     verbose = False
     overwrite = False
 
-    with open(config_filename, "r") as op:
+    with open(args["config"], "r") as op:
         data = json.load(op)
         for key in data:
             subject_ids.extend(data[key])
@@ -76,18 +86,18 @@ def main():
         left_atlas_roi = atlas_roi_template.format(subject=subject, hemi="L")
         right_atlas_roi = atlas_roi_template.format(subject=subject, hemi="R")
         vol = volume_template.format(subject=subject)
-        filename = vol.replace(".nii.gz", ".{}.dscalar.nii".format(surface_name))
+        filename = vol.replace(".nii.gz", ".{}.dscalar.nii".format(args["surface_name"]))
         if os.path.exists(vol) and (overwrite or not os.path.exists(filename)):
             for surf, hemi in ((left_surf, "L"), (right_surf, "R")):
                 if not os.path.exists(surf):
-                    download(os.path.join("s3://hcp-openaccess/{}/".format(os.path.basename(directory)),
+                    download(os.path.join("s3://hcp-openaccess/{}/".format(os.path.basename(args["hcp_dir"])),
                                           subject,
                                           os.path.dirname(surface_template)),
-                             os.path.join(directory, subject, os.path.dirname(surface_template)),
+                             os.path.join(args["hcp_dir"], subject, os.path.dirname(surface_template)),
                              verbose=verbose,
                              include=[os.path.basename(surf)])
-            volume_to_surface(vol, left_surf, right_surf, filename, surface_name, method=method,
-                              name_file=name_file, verbose=verbose,
+            volume_to_surface(vol, left_surf, right_surf, filename, args["surface_name"], method=args["method"],
+                              name_file=args["task_names"], verbose=verbose,
                               right_atlas_roi=right_atlas_roi, left_atlas_roi=left_atlas_roi)
         elif not os.path.exists(vol):
             if verbose:

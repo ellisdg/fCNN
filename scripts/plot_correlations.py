dc466678c41f595d8c6cb2eab6cbea77c978cd93
ellisdg <dgellis90@gmail.com>
2020-06-25 09:48:48 -0500

    ENH: compare correlations

--- scripts/plot_correlations.py
+++ scripts/plot_correlations.py
@@ -12,7 +12,8 @@ import pandas as pd
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--output_dir', required=True)
-    parser.add_argument('--correlation_filename', required=True)
+    parser.add_argument('--correlation_filename', nargs="+", required=True)
+    parser.add_argument('--labels', nargs="*")
     parser.add_argument('--task_names', default="/home/aizenberg/dgellis/fCNN/data/labels/ALL-TAVOR_name-file.txt")
     parser.add_argument('--level', default="overall", choices=["overall", "domain", "task"])
     parser.add_argument('--stats_filename', default=None)
@@ -150,6 +151,78 @@ def plot_correlation_panel(corr_matrix, column_width=3, row_height=3, norm_vmax=
     save_fig(overall_all_fig, output_dir + "/correlation_overall_panel", bbox_inches="tight")
 
 
+def mean_correlations(array, axis=None):
+    # transform the correlations (r) into z values
+    z = np.arctanh(array)
+    # take the mean of the z values
+    z_mean = np.mean(z, axis=axis)
+    # convert the mean back into r values
+    return np.tanh(z_mean)
+
+
+def self_vs_other_correlation(corr_matrix):
+    diagonal_mask = np.diag(np.ones(corr_matrix.shape[0], dtype=bool))
+    diag_values = corr_matrix[diagonal_mask]
+    extra_diag_values = corr_matrix[diagonal_mask == False]
+    return ((mean_correlations(diag_values) - mean_correlations(extra_diag_values))/
+            mean_correlations(extra_diag_values)) * 100
+
+
+def plot_self_vs_other_correlations(corr_matrices, model_labels, method_labels, output_dir,
+                                    metric_func=self_vs_other_correlation,
+                                    output_filename="{}_increase_correlation_over_mean_correlation",
+                                    xlabel="Self vs other increase (in %)"):
+    result = list()
+    for i, (model_name, method_name) in enumerate(zip(model_labels, method_labels)):
+        corr_matrix = corr_matrices[i]
+        value = metric_func(corr_matrix)
+        result.append([model_name, method_name, value])
+    df = pd.DataFrame(result, columns=["Model", "Method", "Value"])
+
+    w = 6
+    width = 0.4
+    gap = 0.
+    h = (width + gap) * len(df)
+    fig, ax = plt.subplots(figsize=(w, h))
+    seaborn.barplot(data=df, x="Value", y="Method", hue="Model", ax=ax)
+    ax.set_xlabel(xlabel)
+    ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    seaborn.despine(ax=ax, top=True)
+    save_fig(fig, os.path.join(output_dir, output_filename.format("_".join(method_labels + model_labels))),
+             bbox_inches="tight")
+
+
+def compare_correlation_models(correlation_files, labels, output_directory):
+    temp_correlations = list()
+    subjects = list()
+    model_labels = list()
+    method_labels = list()
+
+    for c_file, label in zip(correlation_files, labels):
+        corr = np.load(c_file)
+        print(c_file, corr.shape, label)
+        subs = np.load(c_file.replace(".npy", "_subjects.npy"))
+        subjects.append(subs)
+        temp_correlations.append(corr)
+        model_label, method_label = label.split("-")
+        method_labels.append(method_label)
+        model_labels.append(model_label)
+
+    all_subjects = reduce(np.intersect1d, subjects)
+    correlations = list()
+    for sub_list, corr in zip(np.copy(subjects), temp_correlations):
+        s, i, i_all = np.intersect1d(sub_list, all_subjects, return_indices=True)
+        np.testing.assert_equal(s, all_subjects)
+        correlations.append(corr[i][:, i])
+    correlations = np.mean(np.asarray(correlations)[..., 0])
+    print(correlations.shape)
+
+    plot_self_vs_other_correlations(correlations, model_labels, method_labels, output_directory,
+         metric_func=self_vs_other_correlation,
+         output_filename="{}_increase_correlation_over_mean_correlation_average",
+         xlabel="Self vs other increase (in %)")
+
+
 def main():
     seaborn.set_palette('muted')
     seaborn.set_style('whitegrid')
@@ -161,8 +234,13 @@ def main():
         stats_filename = args["stats_filename"]
 
     if args["level"] == "overall":
-        corr_matrix = np.load(args["correlation_filename"])[..., 0]
-        plot_correlation_panel(corr_matrix=corr_matrix, output_dir=args["output_dir"])
+        if len(args["correlation_filename"]) > 1:
+            # compare correlations
+            compare_correlation_models(args["correlation_filename"], args["labels"], args["output_dir"])
+        else:
+            # plot correlation results for a single correlation file
+            corr_matrix = np.load(args["correlation_filename"])[..., 0]
+            plot_correlation_panel(corr_matrix=corr_matrix, output_dir=args["output_dir"])
     else:
         raise NotImplementedError("Level={}".format(args["level"]))
 

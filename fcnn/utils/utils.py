FIX: fix masking

fcnn/utils/utils.py
@@ -287,5 +287,5 @@ def extract_sub_volumes(image, sub_volume_indices):
     return new_img_like(ref_niimg=image, data=data)
 
 
-def mask(data, threshold=0):
-    return np.ones(data > threshold)
+def mask(data, threshold=0, dtype=np.float):
+    return np.asarray(data > threshold, dtype=dtype)
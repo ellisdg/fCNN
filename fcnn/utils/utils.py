50898ab50af1c8eb2e421826913e7eac004d92d0
ellisdg <dgellis90@gmail.com>
2020-07-07 08:30:26 -0500

    FIX: fix masking

--- fcnn/utils/utils.py
+++ fcnn/utils/utils.py
@@ -287,5 +287,5 @@ def extract_sub_volumes(image, sub_volume_indices):
     return new_img_like(ref_niimg=image, data=data)
 
 
-def mask(data, threshold=0):
-    return np.ones(data > threshold)
+def mask(data, threshold=0, dtype=np.float):
+    return np.asarray(data > threshold, dtype=dtype)

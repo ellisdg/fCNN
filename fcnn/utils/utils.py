FIX: trying to fix the normalization

fcnn/utils/utils.py
@@ -95,7 +95,10 @@ def zero_one_window(data, axis=(0, 1, 2), ceiling_percentile=99, floor_percentil
         floor_threshold = np.percentile(data, floor_percentile, axis=axis)
         if channels_axis is None:
             for i in range(data.ndim):
-                if i not in axis and (i - data.ndim) not in axis:  # I don't understand the second part of this
+                if i not in axis and (i - data.ndim) not in axis:
+                    # I don't understand the second part of this if statement
+                    # (answer) it is checking ot make sure that the axis is not indexed in reverse (i.e. axis 3 might be
+                    # indexed as -1)
                     channels_axis = i
         data = np.moveaxis(data, channels_axis, 0)
         for channel in range(data.shape[0]):
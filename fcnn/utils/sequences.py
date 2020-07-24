FIX: normalization

fcnn/utils/sequences.py
@@ -50,7 +50,7 @@ def normalize_image_with_function(image, function, volume_indices=None, **kwargs
     if volume_indices is not None:
         data[..., volume_indices] = function(data[..., volume_indices], **kwargs)
     else:
-        data[:] = function(data[:], **kwargs)
+        data = function(data, **kwargs)
     return new_img_like(image, data=data, affine=image.affine)
 
 
@@ -74,7 +74,6 @@ def normalize_data_with_multiple_functions(data, normalization_names, channels_a
         else:
             _data = data
         normalized_data.append(func(_data, **kwargs[name]))
-    print([d.shape for d in normalized_data])
     return np.concatenate(normalized_data, axis=channels_axis)
 
 
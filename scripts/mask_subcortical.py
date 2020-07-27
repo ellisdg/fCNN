afe0c952cf6748fbfa747bc00e4bd04be5fd5097
ellisdg <dgellis90@gmail.com>
2020-07-27 08:10:35 -0500

    ENH: allows to make cortex masks too

--- scripts/mask_subcortical.py
+++ scripts/mask_subcortical.py
@@ -15,11 +15,12 @@ def parse_args():
     parser.add_argument('--input_filename', default="T1w_acpc_dc_restore_brain.nii.gz")
     parser.add_argument('--output_filename', default="T1w_acpc_dc_restore_subcortical.nii.gz")
     parser.add_argument('--nthreads', type=int, default=1)
+    parser.add_argument('--cortex', action="store_true", default=False)
     parser.add_argument('--overwrite', action="store_true", default=False)
     return vars(parser.parse_args())
 
 
-def mask_t1_image(subject, hcp_dir, input_filename, output_filename, overwrite=False):
+def mask_t1_image(subject, hcp_dir, input_filename, output_filename, overwrite=False, cortex=False):
     output_filename = os.path.join(hcp_dir, subject, "T1w", output_filename)
     t1_filename = os.path.join(hcp_dir, subject, "T1w", input_filename)
     if (overwrite or not os.path.exists(output_filename)) and os.path.exists(t1_filename):
@@ -30,10 +31,13 @@ def mask_t1_image(subject, hcp_dir, input_filename, output_filename, overwrite=F
         atlas = aparc.get_data()
         exclude = [46, 47, 7, 8, 15, 16]
         excluded_mask = np.in1d(atlas.flat, exclude)
-        not_cortex = np.logical_and(atlas.flat < 1000, atlas.flat > 0)
-        not_cortex[excluded_mask] = False
+        if cortex:
+            also_excluded = atlas.flat > 1000
+        else:
+            also_excluded = np.logical_and(atlas.flat < 1000, atlas.flat > 0)
+        also_excluded[excluded_mask] = False
         t1_data = t1.get_fdata()
-        t1_data.flat[not_cortex == False] = 0.
+        t1_data.flat[also_excluded == False] = 0.
         wm_t1 = t1.__class__(dataobj=t1_data, header=t1.header, affine=t1.affine)
         wm_t1.to_filename(output_filename)
 

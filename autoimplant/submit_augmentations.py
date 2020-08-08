00b1c5bd743650b53218862f856796c29a384238
ellisdg <dgellis90@gmail.com>
2020-08-08 09:35:24 -0500

    ENH: checks queue and waits

--- autoimplant/submit_augmentations.py
+++ autoimplant/submit_augmentations.py
@@ -1,6 +1,18 @@
 import subprocess
 import glob
 import os
+import io
+import time
+
+
+def check_queue_length():
+    proc = subprocess.Popen(["squeue", "-u", "dgellis"], stdout=subprocess.PIPE)
+    return len(list(io.TextIOWrapper(proc.stdout, encoding="utf-8")))
+
+
+def wait_for_long_queue(sleeping_time=60):
+    while check_queue_length() > 1000:
+        time.sleep(sleeping_time)
 
 
 def main():
@@ -10,6 +22,7 @@ def main():
     for case in cases:
         cases2.pop(cases.index(case))
         for case2 in cases:
+            wait_for_long_queue()
             subprocess.call(["sbatch /home/aizenberg/dgellis/fCNN/autoimplant/augmentation_script.sh", case, case2])
 
 

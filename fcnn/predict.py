413ef988ff8c25c77995acba8c69c005c83711ee
ellisdg <dgellis90@gmail.com>
2020-08-10 14:53:24 -0500

    FIX

--- fcnn/predict.py
+++ fcnn/predict.py
@@ -609,14 +609,13 @@ def predict_with_permutations(model, data, n_outputs, batch_size, n_gpus, permut
     prediction_data = np.zeros((len(permutation_keys),) + data.shape[:3] + (n_outputs,))
     batch = list()
     permutation_indices = list()
-    print("Data:", data.shape)
-    data = np.moveaxis(data, 3, 1)
-    print("Data:", data.shape)
+    data = np.moveaxis(data, 3, 0)
     for permutation_idx, permutation_key in enumerate(permutation_keys):
         batch.append(permute_data(data, permutation_key))
         permutation_indices.append(permutation_idx)
         if len(batch) >= batch_size or permutation_key == permutation_keys[-1]:
-            batch_prediction = pytorch_predict_batch(torch.tensor(batch).float(), model, n_gpus).numpy()
+            batch_prediction = np.moveaxis(pytorch_predict_batch(torch.tensor(batch).float(), model, n_gpus).numpy(),
+                                           1, 4)
             for batch_idx, perm_idx in enumerate(permutation_indices):
                 prediction_data[perm_idx] = batch_prediction[batch_idx].squeeze()
             batch = list()

57d18c1d54cdbe887bdf121ddffb7691aabbed3b
ellisdg <dgellis90@gmail.com>
2020-08-12 17:59:44 -0500

    ENH: submit predictions

--- slurm/submit_predictions.py
+++ slurm/submit_predictions.py
@@ -18,7 +18,8 @@ def parse_args():
 def format_process(**kwargs):
     process = "python fcnn/scripts/run_unet_inference.py"
     for key, value in kwargs.items():
-        process = "{process} --{key} {value}".format(process=process, key=key, value=value)
+        if value:
+            process = "{process} --{key} {value}".format(process=process, key=key, value=value)
     return process
 
 

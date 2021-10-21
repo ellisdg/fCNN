6790d71dc9bd8d033b2a610296a5e5218acede15
david.ellis <david.ellis@unmc.edu>
2021-10-21 11:16:55 -0500

    ENH: adds tfMRI transform script

--- scripts/bash/transform_tfMRI_volumes.sh
+++ scripts/bash/transform_tfMRI_volumes.sh
@@ -6,7 +6,7 @@ SUBDIR=${HCPDIR}/${SUB}
 TFMRIVOL=${SUBDIR}/T1w/Results/tfMRI_ALL/tfMRI_ALL_hp200_s2_level2.feat/${SUB}_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz
 XFM=${SUBDIR}/MNINonLinear/xfms/acpc_dc2standard.nii.gz
 REF=${SUBDIR}/MNINonLinear/T1w_restore.2.nii.gz
-REF2=${SUBDIR}/T1w/T1w_acpcp_dc_restore_brain.nii.gz
+REF2=${SUBDIR}/T1w/T1w_acpc_dc_restore_brain.nii.gz
 OUTDIR=${SUBDIR}/MNINonLinear/Results/tfMRI_ALL
 
 mkdir -p "${OUTDIR}"

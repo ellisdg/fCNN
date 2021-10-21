9e35e539a3a3729eb4b61facff8d800150e78677
david.ellis <david.ellis@unmc.edu>
2021-10-21 11:05:34 -0500

    ENH: adds tfMRI transform script

---
+++ scripts/bash/transform_tfMRI_volumes.sh
@@ -0,0 +1,20 @@
+HCPDIR=${WORK}/HCP/HCP_1200
+SUB=${1}
+SUBDIR=${HCPDIR}/${SUB}
+TFMRIVOL=${SUBDIR}/T1w/Resluts/tfMRI_ALL/tfMRI_ALL_hp200_s2_level2.feat/${SUB}_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz
+XFM=${SUBDIR}/MNINonLinear/xfms/acpc_dc2standard.nii.gz
+REF=${SUBDIR}/MNINonLinear/T1w_restore.2.nii.gz
+REF2=${SUBDIR}/T1w/T1w_acpcp_dc_restore_brain.nii.gz
+OUTDIR=${SUBDIR}/MNINonLinear/Results/tfMRI_ALL
+
+mkdir -p "${OUTDIR}"
+
+OUTVOL=${OUTDIR}/${SUB}_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz
+
+wb_command -volume-warpfield-resample\
+ "${TFMRIVOL}"\
+ "${XFM}"\
+ "${REF}"\
+ ENCLOSING_VOXEL\
+ "${OUTVOL}"\
+ -fnirt "${REF2}"

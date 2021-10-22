f6e9a23cbe0bfc58bd3e9f757883acb654dd782d
david.ellis <david.ellis@unmc.edu>
2021-10-22 09:55:59 -0500

    ENH: partial volume script

---
+++ scripts/bash/compute_partial_volume_correlations_32cpus.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --time=7-00:00:00
+#SBATCH --job-name=correlations
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem-per-cpu=8000
+#SBATCH --error=/work/aizenberg/dgellis/fCNN/logs/job.%J.err
+#SBATCH --output=/work/aizenberg/dgellis/fCNN/logs/job.%J.out
+
+module load anaconda
+conda activate fcnn-1.12
+export PYTHONPATH=$HOME/fCNN:$PYTHONPATH
+target=${1}
+partial=${2}
+
+python $HOME/fCNN/scripts/compile_predicted_v_actual_overall_correlations.py --output_dir $WORK/fCNN/predictions/v4_${target}_unet_ALL-TAVOR_2mm_v2_pt_test --output_filename $WORK/fCNN/predictions/v4_${target}_unet_ALL-TAVOR_2mm_v2_pt_test/overall_partial_cov_${partial}_volume_correlations.npy --config_filename $HOME/fCNN/data/v4_${target}_unet_ALL-TAVOR_2mm_v2_pt_config.json --nthreads 32 --verbose --covariate_dir $WORK/fCNN/predictions/v4_${partial}_unet_ALL-TAVOR_2mm_v2_pt_test --volume

14d24b8a43a81259e71b4c09e4281a293c672236
David Ellis <david.ellis@unmc.edu>
2022-01-28 14:42:49 -0600

    ENH: writes roi restricted ciftis

--- scripts/apply_roi_to_cifti.py
+++ scripts/apply_roi_to_cifti.py
@@ -8,7 +8,7 @@ def main():
     r_roi_wc = "/work/aizenberg/dgellis/HCP/HCP_1200/{0}/MNINonLinear/fsaverage_LR32k/{0}.R.atlasroi.32k_fs_LR.shape.gii"
     for fn in glob.glob("/work/aizenberg/dgellis/HCP/HCP_1200/*/T1w/Results/tfMRI_ALL/tfMRI_ALL_hp200_s2_level2.feat/*_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.midthickness.dscalar.nii"):
         out_file = fn.replace(".midthickness.", ".roi.midthickness.")
-        if not os.path.exists:
+        if not os.path.exists(out_file):
             subject = fn.split("")[6]
             cmd = ["wb_command",
                    "-cifti-restrict-dense-map",

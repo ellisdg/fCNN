f8f60d830a9a22b14a9346fd946a88333b904d40
David Ellis <david.ellis@unmc.edu>
2022-02-08 15:41:12 -0600

    ENH: adds arguments to script

--- scripts/transform_tFMRI_volumes.py
+++ scripts/transform_tFMRI_volumes.py
@@ -1,10 +1,19 @@
 from fcnn.utils.utils import load_json
 import subprocess
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--group", default="test")
+    parser.add_argument("--subjects_file", default="/home/aizenberg/dgellis/fCNN/data/subjects_v4.json")
+    return parser.parse_args()
 
 
 def main():
-    subjects = load_json("/home/aizenberg/dgellis/fCNN/data/subjects_v4.json")
-    for subject in subjects["test"]:
+    namespace = parse_args()
+    subjects = load_json(namespace.subjects_file)
+    for subject in subjects[namespace.group]:
         subprocess.call(["bash", "/home/aizenberg/dgellis/fCNN/scripts/bash/transform_tfMRI_volumes.sh", subject])
 
 

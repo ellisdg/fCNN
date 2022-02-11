470d0368cd734afa5f12b9732d3db6725bea21bc
David Ellis <david.ellis@unmc.edu>
2022-02-11 17:45:21 -0600

    ENH: make group average volume

--- scripts/make_average_image.py
+++ scripts/make_average_image.py
@@ -6,6 +6,7 @@ def parse_args():
     parser.add_argument("--group", default="training")
     parser.add_argument("--config_filename",
                         default="/home/aizenberg/dgellis/fCNN/data/subjects_v4-retest.json")
+    parser.add_argument("--overwrite", default=False, action="store_true")
     return parser.parse_args()
 
 
@@ -16,28 +17,31 @@ def main():
 
     namespace = parse_args()
 
-    subjects = load_json(namespace.config_filename)[namespace.group]
-    data = None
-    filenames = list()
-    for subject in sorted(subjects):
-        fn = "/work/aizenberg/dgellis/HCP/HCP_1200/{0}/MNINonLinear/Results/tfMRI_ALL/tfMRI_ALL_hp200_s2_level2.feat/{0}_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz".format(subject)
-        if os.path.exists(fn):
-            filenames.append(fn)
-    n = len(filenames)
-    print(n)
+    output_filename = "/work/aizenberg/dgellis/fCNN/{}_{}_average_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz".format(namespace.group,
+                                                                                                                       os.path.basename(namespace.config_filename).split(".")[0])
 
-    for fn in filenames:
-        print(fn)
-        image = nib.load(fn)
-        _data = image.get_fdata()/n
-        if data is None:
-            data = _data
-        else:
-            data = data + _data
+    if namespace.overwrite or not os.path.exists(output_filename):
+        subjects = load_json(namespace.config_filename)[namespace.group]
+        data = None
+        filenames = list()
+        for subject in sorted(subjects):
+            fn = "/work/aizenberg/dgellis/HCP/HCP_1200/{0}/MNINonLinear/Results/tfMRI_ALL/tfMRI_ALL_hp200_s2_level2.feat/{0}_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz".format(subject)
+            if os.path.exists(fn):
+                filenames.append(fn)
+        n = len(filenames)
+        print(n)
 
-    new_image = new_img_like(data=data, ref_niimg=image)
-    new_image.to_filename("/work/aizenberg/dgellis/fCNN/{}_{}_average_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz".format(namespace.group,
-                                                                                                                           os.path.basename(namespace.config_filename).split(".")[0]))
+        for fn in filenames:
+            print(fn)
+            image = nib.load(fn)
+            _data = image.get_fdata()/n
+            if data is None:
+                data = _data
+            else:
+                data = data + _data
+
+        new_image = new_img_like(data=data, ref_niimg=image)
+        new_image.to_filename(output_filename)
 
 
 if __name__ == "__main__":

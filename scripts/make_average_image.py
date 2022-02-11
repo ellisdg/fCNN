389f56e442556b0f40887fea5bc2b7777c06791f
David Ellis <david.ellis@unmc.edu>
2022-02-11 17:19:30 -0600

    ENH: make group average volume

--- scripts/make_average_image.py
+++ scripts/make_average_image.py
@@ -1,23 +1,30 @@
+import argparse
+import os
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--group", default="training")
+    parser.add_argument("--config_filename",
+                        default="/home/aizenberg/dgellis/fCNN/data/subjects_v4-retest.json")
+    return parser.parse_args()
+
+
 def main():
-    import os
     import nibabel as nib
     from fcnn.utils.utils import load_json
     from nilearn.image import new_img_like
 
-    subjects = load_json("/home/aizenberg/dgellis/fCNN/data/subjects_v4-retest.json")["training"]
+    namespace = parse_args()
+
+    subjects = load_json(namespace.config_filename)[namespace.group]
     data = None
     filenames = list()
     for subject in sorted(subjects):
-        fn1 = "/work/aizenberg/dgellis/HCP/HCP_1200/{0}/MNINonLinear/Results/tfMRI_ALL/{0}_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz".format(subject)
-        fn2 = "/work/aizenberg/dgellis/HCP/HCP_1200/{0}/MNINonLinear/Results/tfMRI_ALL/tfMRI_ALL_hp200_s2_level2.feat/{0}_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz".format(subject)
-        if os.path.exists(fn1):
-            fn = fn1
-        elif os.path.exists(fn2):
-            fn = fn2
-        else:
-            continue
-        filenames.append(fn)
+        fn = "/work/aizenberg/dgellis/HCP/HCP_1200/{0}/MNINonLinear/Results/tfMRI_ALL/tfMRI_ALL_hp200_s2_level2.feat/{0}_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz".format(subject)
+        if os.path.exists(fn):
+            filenames.append(fn)
     n = len(filenames)
+    print(n)
 
     for fn in filenames:
         print(fn)
@@ -29,7 +36,8 @@ def main():
             data = data + _data
 
     new_image = new_img_like(data=data, ref_niimg=image)
-    new_image.to_filename("/work/aizenberg/dgellis/fCNN/v4_training_average_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz")
+    new_image.to_filename("/work/aizenberg/dgellis/fCNN/{}_{}_average_tfMRI_ALL_level2_zstat_hp200_s2_TAVOR.nii.gz".format(namespace.group,
+                                                                                                                           os.path.basename(namespace.config_filename).split(".")[0]))
 
 
 if __name__ == "__main__":

d81ce8ab1173d136742eb7b6c2edf7783c9d3e56
David Ellis <david.ellis@unmc.edu>
2022-02-14 11:14:20 -0600

    ENH: adds multiprocessing

--- scripts/transform_tFMRI_volumes.py
+++ scripts/transform_tFMRI_volumes.py
@@ -1,20 +1,28 @@
 from fcnn.utils.utils import load_json
 import subprocess
 import argparse
+from multiprocessing import Pool
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--group", default="test")
     parser.add_argument("--subjects_file", default="/home/aizenberg/dgellis/fCNN/data/subjects_v4.json")
+    parser.add_argument("--nthreads", type=int, default=1)
     return parser.parse_args()
+
+
+def transform_subject(subject):
+    cmd = ["bash", "/home/aizenberg/dgellis/fCNN/scripts/bash/transform_tfMRI_volumes.sh", subject]
+    print(" ".join(cmd))
+    subprocess.call(["bash", "/home/aizenberg/dgellis/fCNN/scripts/bash/transform_tfMRI_volumes.sh", subject])
 
 
 def main():
     namespace = parse_args()
-    subjects = load_json(namespace.subjects_file)
-    for subject in subjects[namespace.group]:
-        subprocess.call(["bash", "/home/aizenberg/dgellis/fCNN/scripts/bash/transform_tfMRI_volumes.sh", subject])
+    subjects = load_json(namespace.subjects_file)[namespace.group]
+    with Pool(namespace.nthreads) as pool:
+        pool.map(transform_subject, subjects)
 
 
 if __name__ == "__main__":

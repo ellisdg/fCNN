70f4e7849b8733d002814443c3711cc2e89b287a
David Ellis <david.ellis@unmc.edu>
2022-02-15 14:43:32 -0600

    ENH: Adds permutation to ablation

--- scripts/bash/plot_overall_ablation_with_linear_model.sh
+++ scripts/bash/plot_overall_ablation_with_linear_model.sh
@@ -6,6 +6,7 @@ python scripts/plot_correlations.py\
  /work/aizenberg/dgellis/fCNN/predictions/v4_wmmask_unet_ALL-TAVOR_2mm_v2_pt_test/overall_correlations.npy\
  /work/aizenberg/dgellis/fCNN/predictions/v4_brainmask_unet_ALL-TAVOR_2mm_v2_pt_test/overall_correlations.npy\
  /work/aizenberg/dgellis/fCNN/regression/predictions/pointwise_60k/overall_correlations.npy\
+ /work/aizenberg/dgellis/fCNN/predictions/v4permute2_struct14_unet_ALL-TAVOR_2mm_v2_pt/overall_correlations.npy\
  /work/aizenberg/dgellis/fCNN/predictions/v4_struct14_unet_ALL-TAVOR_2mm_v2_pt_test/overall_correlations_MSMAll.npy\
  /work/aizenberg/dgellis/fCNN/predictions/v4_t1t2_unet_ALL-TAVOR_2mm_v2_pt_test/overall_correlations_MSMAll.npy\
  /work/aizenberg/dgellis/fCNN/predictions/v4_t1_unet_ALL-TAVOR_2mm_v2_pt_test/overall_correlations_MSMAll.npy\
@@ -20,6 +21,6 @@ python scripts/plot_correlations.py\
  /work/aizenberg/dgellis/fCNN/predictions/v4_wmmask_unet_ALL-TAVOR_2mm_v2_pt_test/overall_correlations_MNINonLinear.npy\
  /work/aizenberg/dgellis/fCNN/predictions/v4_brainmask_unet_ALL-TAVOR_2mm_v2_pt_test/overall_correlations_MNINonLinear.npy\
  --output_dir /work/aizenberg/dgellis/fCNN/predictions/figures/test/weighted/struct14/with_linear_model\
- --labels DTI+T1+T2-MSMSulc T1+T2-MSMSulc T1-MSMSulc "Cortex Mask-MSMSulc" "Subcortical Mask-MSMSulc" "Brain Mask-MSMSulc" "Linear Model-MSMSulc"\
+ --labels DTI+T1+T2-MSMSulc T1+T2-MSMSulc T1-MSMSulc "Cortex Mask-MSMSulc" "Subcortical Mask-MSMSulc" "Brain Mask-MSMSulc" "Linear Model-MSMSulc" "Permutation-MSMSulc"\
  DTI+T1+T2-MSMAll T1+T2-MSMAll T1-MSMAll "Cortex Mask-MSMAll" "Subcortical Mask-MSMAll" "Brain Mask-MSMAll" "Linear Model-MSMAll"\
  DTI+T1+T2-MNINonLinear T1+T2-MNINonLinear T1-MNINonLinear "Cortex Mask-MNINonLinear" "Subcortical Mask-MNINonLinear" "Brain Mask-MNINonLinear"
